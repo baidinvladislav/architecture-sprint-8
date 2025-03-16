e4136351254495e880edc22714e803c2f87bb457
baydinvladislav <baydinvladislav@yandex.ru>
2025-03-16 12:31:52 +0300

    add logs

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,7 +1,11 @@
+import logging
+
 from fastapi import Security, HTTPException
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from keycloak import KeycloakOpenID
 from starlette import status
+
+logger = logging.Logger(name=__file__)
 
 oauth2_scheme = OAuth2AuthorizationCodeBearer(
     authorizationUrl="http://keycloak:8080/realms/reports-realm/protocol/openid-connect/auth",
@@ -17,21 +21,28 @@ keycloak_openid = KeycloakOpenID(
 
 
 def get_payload(token: str = Security(oauth2_scheme)) -> dict:
+    logger.info("Starting access check.")
+
     try:
         payload = keycloak_openid.decode_token(token)
+        logger.info("Payload parsed successfully.")
     except Exception as e:
+        logger.error("Token validation error occurred: %s", e)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Token validation error: {str(e)}",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+    logger.info("Checking for 'prothetic_user' role.")
     roles = payload.get("realm_access", {}).get("roles", [])
     if "prothetic_user" not in roles:
+        logger.warning("User does not have the 'prothetic_user' role.")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Insufficient permissions",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+    logger.info("User has the 'prothetic_user' role. Access granted.")
     return payload
