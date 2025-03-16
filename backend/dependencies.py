363d89b6ef5372765defaada82913002512a7c0f
baydinvladislav <baydinvladislav@yandex.ru>
2025-03-16 11:59:06 +0300

    raise 401 status code when user without token

---
+++ backend/dependencies.py
@@ -0,0 +1,37 @@
+from fastapi import Security, HTTPException
+from fastapi.security import OAuth2AuthorizationCodeBearer
+from keycloak import KeycloakOpenID
+from starlette import status
+
+oauth2_scheme = OAuth2AuthorizationCodeBearer(
+    authorizationUrl="http://keycloak:8080/realms/reports-realm/protocol/openid-connect/auth",
+    tokenUrl="http://keycloak:8080/realms/reports-realm/protocol/openid-connect/token",
+)
+
+keycloak_openid = KeycloakOpenID(
+    server_url="http://keycloak:8080/",
+    client_id="reports-backend",
+    realm_name="reports-realm",
+    client_secret_key="supersecret",
+)
+
+
+def get_payload(token: str = Security(oauth2_scheme)) -> dict:
+    try:
+        payload = keycloak_openid.decode_token(token)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Token validation error: {str(e)}",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    roles = payload.get("realm_access", {}).get("roles", [])
+    if "prothetic_user" not in roles:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Insufficient permissions",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return payload
---
+++ backend/settings.py
@@ -0,0 +1,8 @@
+class AppSettings:
+    keycloak_settings_url: str
+    keycloak_settings_client_id: str
+    keycloak_settings_realm: str
+    keycloak_settings_client_secret: str
+
+
+app_settings = AppSettings()
