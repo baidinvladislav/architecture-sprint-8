26de6a5f87e564f72c283b0edbe9acc84979529a
baydinvladislav <baydinvladislav@yandex.ru>
2025-03-16 11:58:26 +0300

    move /reports router

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,19 @@
-from datetime import datetime
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from routers import report_router
 
 app = FastAPI()
 
+app.include_router(report_router)
 
-class Report(BaseModel):
-    id: int
-    title: str
-    content: str
-    created_at: str
+origins = [
+    "http://localhost:3000",
+]
 
-
-@app.get("/reports", response_model=list[Report])
-def get_reports():
-    reports = []
-    for i in range(5):
-        report = Report(
-            id=i,
-            title=f"Отчёт #{i}",
-            content=f"Содержимое отчёта #{i}",
-            created_at=datetime.now().isoformat()
-        )
-        reports.append(report)
-
-    return reports
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
